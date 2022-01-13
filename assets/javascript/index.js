c7bd8642e65b33181ee5864f9b1c3a730f2cf3ac
Luiz Felipe Sacardo <luizfelipe_1992@hotmail.com>
2022-01-13 07:14:47 -0300

    Corrected the request URL

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,18 +1,29 @@
-function ZipConsultation(){ 
+function ZipConsultation(event){ 
+  event.preventDefault();
+
   const inputZipCode = document.querySelector('#form__zip-code').value;
   const zipCode = inputZipCode.replace("-", "");
   
-  const url = 'https://viacep.com.br/'+ zipCode +'/json/';  
+  const url = 'https://viacep.com.br/ws/'+ zipCode +'/json/';  
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);  
-  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+  xhr.open('GET', url, true);  
 
 xhr.onreadystatechange = function(){
   if(xhr.readyState == 4){
   if(xhr.status == 200){
-    console.log(xhr.responseText);
-    }
-  }     
+    fillFields(JSON.parse(xhr.responseText));
+  }       
 }
 xhr.send();
 }
+console.log(xhr.readyState, xhr.status)
+}
+
+
+function fillFields(json) { 
+  document.querySelector('#form__street').value = json.logradouro;
+  document.querySelector('#form__number').value = '-';
+  document.querySelector('#form__district').value = json.bairro; 
+  document.querySelector('#form__complement').value = json.complemento;
+  document.querySelector('#form__city').value = json.localidade; 
+  document.querySelector('#form__state').value = json.uf; }
